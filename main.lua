0bb2ddcd8c6db9061a62396c118482bc8ed0c18b
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-04-10 15:55:28 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,8 @@
+-- Testmod v1.0.0
+-- SmoothSpatula
+
+log.info("Successfully loaded ".._ENV["!guid"]..".")
+
 -- Work In Progress
 -- SmoothSpatula
 
@@ -11,38 +16,64 @@ wrd = '%s*(%a+)%s*' -- word
 -- <> is a mandatory field
 -- [] is an optional field
 match_strings = {
-  kill_username = '/kill'..usrn, -- /kill [username]
-  give_item_id = '/give'..nb..usrn..nb, -- /give <item_id> <username> [amount]
-  give_item_name = '/give'..wrd..usrn..nb, -- /give <item_name> <username> [amount]
-  remove_item_id = '/remove'..nb..usrn..nb, -- /remove <item_id> <username> [amount]
-  remove_item_name = '/remove'..wrd..usrn..nb, -- /remove <item_name> <username> [amount]
-  set_field = '/set'..wrd..nb..usrn, -- /set <field_name> <value> [username]
-  set_skill_id = '/set'..nb..nb..usrn, -- /skill <bar_slot> <skill_id> [username]
-  set_skill_name = '/set'..nb..wrd..usrn, -- /skill <bar_slot> <skill_name> [username]
-  spawn_tp = '/spawntp' -- /spawntp
+    kill_username = '/kill'..usrn, -- /kill [username]
+    give_item_id = '/give'..nb..usrn..nb, -- /give <item_id> <username> [amount]
+    give_item_name = '/give'..wrd..usrn..nb, -- /give <item_name> <username> [amount]
+    remove_item_id = '/remove'..nb..usrn..nb, -- /remove <item_id> <username> [amount]
+    remove_item_name = '/remove'..wrd..usrn..nb, -- /remove <item_name> <username> [amount]
+    set_field = '/set'..wrd..nb..usrn, -- /set <field_name> <value> [username]
+    set_skill_id = '/set'..nb..nb..usrn, -- /skill <bar_slot> <skill_id> [username]
+    set_skill_name = '/set'..nb..wrd..usrn, -- /skill <bar_slot> <skill_name> [username]
+    spawn_tp = '/spawntp' -- /spawntp
 }
 
--- ========== Commands ==========
+-- ========== Functions Associated With Commands ==========
 
-function kill_username(username)
-    if username == '' then
-      print("kill your own character")
-    else
-      print("kill "..username)
+functions = {
+    kill_username = function(username)
+        if username == '' then
+            print("killed your own character")
+        else
+            print("kill "..username)
+        end
     end
+}
+
+-- add functions after
+functions['give_item_name'] = function(item_name, username, amount)
+    if amount == nil then amount = 1 end
+    print( "given "..amount.." "..item_name.." to "..username)
 end
 
-function give_item_name(item_name, username, amount)
-  if amount == nil then amount = 1 end
-  print( "given "..amount.." "..item_name.." to "..username)
-end
+-- ========== ImGui ==========
+
+-- Console in ImGui, Work In Progress
+local text = ""
+gui.add_imgui(function()
+    if ImGui.Begin("Console") then
+        text, selected = ImGui.InputText("", text, 200)
+        if selected then 
+            print(text)
+        end
+        ImGui.TextColored(0.5, 0, 0, 1, text)
+    end
+end)
 
 -- ========== Main ==========
 
-command = "/give GoatHoof Umigatari 12"
+--command = "/give GoatHoof Umigatari 12"
 --command = "/kill"
-for command_name, pattern in pairs(match_strings) do
-  if string.match(command, pattern) then
-    _G[command_name] (string.match(command, pattern))
-  end
+
+function match_command(actor, command)
+    for command_name, pattern in pairs(match_strings) do
+        if string.match(command, pattern) then
+            functions[command_name] (string.match(command, pattern))
+            return 0
+        end
+    end
+    return 1
 end
+
+gm.post_script_hook(gm.constants.chat_add_user_message, function(self, other, result, args)
+    match_command(args[1].value, args[2].value) -- pass actor instance and message
+end)
