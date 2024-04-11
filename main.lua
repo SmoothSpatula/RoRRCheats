2ff4cf9fcff74908dfbcf8dd80961b8a8b7c2247
Gabriel Combe-Ounkham <gabrielcombe@gmail.com>
2024-04-11 18:45:48 +0100

    Update main.lua, add functions for the commands

--- main.lua
+++ main.lua
@@ -8,13 +8,14 @@ log.info("Successfully loaded ".._ENV["!guid"]..".")
 
 -- ========== Parameters ==========
 
-o_user = '%s*([^%s]*)%s*' -- optional username
-m_user = '%s*([^%s]+)%s*' -- mandatory username
-o_nb = '%s*(%d*)%s*' -- optional number
-m_nb = '%s*(%d+)%s*' -- mandatory number
-o_wrd = '%s*(%a*)%s*' -- optional word
-m_wrd = '%s*(%a+)%s*' -- mandatory word
+local lvlToGive = 0
 
+o_user = '%s*([^%s]*)' -- optional username
+m_user = '%s+([^%s]+)' -- mandatory username
+o_nb = '%s*(%d*)' -- optional number
+m_nb = '%s+(%d+)' -- mandatory number
+o_wrd = '%s*(%w*)' -- optional word
+m_wrd = '%s+(%w+)' -- mandatory word
 
 
 -- <> is a mandatory field
@@ -25,24 +26,24 @@ match_strings = {
         usage =  '<y>/kill <b>username',
         help_str = 'kill'
     },
-    give_item_id = { 
-        regex = '/give'..o_nb..m_nb..o_user, 
-        usage = '<y>/give <b>amount <w>item_id <b>username',
+    give_item = { 
+        regex = '/give'..m_nb..m_wrd..o_user, 
+        usage = '<y>/give <b>amount <w>item_name or item_id <b>username',
         help_str = 'give'
     },
-    give_item_name = { 
-        regex = '/give'..o_nb..m_wrd..o_user, 
-        usage = '<y>/give <b>amount <w>item_name <b>username',
-        help_str = 'give'
+    give_gold = {
+        regex = '/gold'..m_nb..o_user,
+        usage = '<y>/gold <w>amount <b>username',
+        help_str = 'gold'
     },
-    remove_item_id = { 
-        regex = '/remove'..o_nb..m_nb..o_user, 
-        usage = '<y>/remove <b>amount <w>item_id <b>username',
-        help_str = 'remove'
+    give_lvl = {
+        regex = '/lvl'..m_nb..o_user,
+        usage = '<y>/lvl <w>number <b>username',
+        help_str = 'lvl'
     },
-    remove_item_name = { 
-        regex = '/remove'..o_nb..m_wrd..o_user, 
-        usage = '<y>/remove <b>amount <w>item_name <b>username',
+    remove_item = { 
+        regex = '/remove'..m_nb..m_wrd..o_user, 
+        usage = '<y>/remove <b>amount <w>item_name or item_id <b>username',
         help_str = 'remove'
     },
     set_field = { 
@@ -118,19 +119,155 @@ end
 
 functions = {
     kill_username = function(actor, username)
-        if username == '' then
+        local killActor = actor
+        if not username then
             print(actor.user_name.. " killed themselves.")
         else
-            print("kill "..username)
+            for i = 1, #gm.CInstance.instances_active do
+                local inst = gm.CInstance.instances_active[i]
+                if inst.object_index == gm.constants.oP and inst.user_name == username then
+                    killActor = inst
+                    break
+                end
+            end
+            print(actor.user_name.." killed "..username)
         end
+        killActor.force_death = 1
+        gm.actor_set_dead(killActor, true)
     end
 }
 
 -- add functions after
-functions['give_item_name'] = function(actor, amount, item_name, username)
-    if amount == '' then amount = 1 end
-    if username == '' then username = actor.user_name end
-    print( "given "..amount.." "..item_name.." to "..username)
+functions['give_item'] = function(actor, amount, item, username)
+    local giveActor = actor
+    if not username then
+        username = actor.user_name
+    else
+        for i = 1, #gm.CInstance.instances_active do
+            local inst = gm.CInstance.instances_active[i]
+            if inst.object_index == gm.constants.oP and inst.user_name == username then
+                giveActor = inst
+                break
+            end
+        end
+    end
+
+    -- Item ID
+    if tonumber(item) then 
+        gm.item_give(giveActor, tonumber(item), tonumber(amount), false)
+        print( "given "..amount.." "..item.." to "..username)
+        return
+    end
+
+    -- Item Name
+    local items = gm.variable_global_get("class_item")
+    for i=1, #items do
+        local lowerItemNameRequest = string.lower(item)
+        local lowerItemName = string.lower(items[i][2])
+
+        if lowerItemNameRequest == lowerItemName or string.find(lowerItemNameRequest, lowerItemName) or string.find(lowerItemName, lowerItemNameRequest) then
+            gm.item_give(giveActor, i-1, tonumber(amount), false)
+            print( "given "..amount.." "..items[i][2].." to "..username)
+            return
+        end
+    end
+
+end
+
+functions['give_gold'] = function(actor, amount, username)
+    local giveActor = actor
+
+    if username then
+        for i = 1, #gm.CInstance.instances_active do
+            local inst = gm.CInstance.instances_active[i]
+            if inst.object_index == gm.constants.oP and inst.user_name == username then
+                giveActor = inst
+                break
+            end
+        end
+    end
+
+    -- giveActor.gold = giveActor.gold + tonumber(amount)
+    -- print(gm.oEfGold.script_name)
+    -- gm.player_get_gold(giveActor, 1000)
+    for i = 1, tonumber(amount) do
+        gm.item_drop_object(gm.constants.oEfGold, giveActor.x, giveActor.y, 0, false)
+    end
+    print(giveActor.gold)
+    print( "given "..amount.." gold to "..giveActor.user_name)
+
+end
+
+functions['give_lvl'] = function(actor, number, username)
+    local giveActor = actor
+
+    if username then
+        for i = 1, #gm.CInstance.instances_active do
+            local inst = gm.CInstance.instances_active[i]
+            if inst.object_index == gm.constants.oP and inst.user_name == username then
+                giveActor = inst
+                break
+            end
+        end
+    end
+    
+    lvlToGive = tonumber(number)
+    print( "given "..number.." level to "..giveActor.user_name)
+
+end
+
+functions['remove_item'] = function(actor, amount, item, username)
+    local giveActor = actor
+    if not username then
+        username = actor.user_name
+    else
+        for i = 1, #gm.CInstance.instances_active do
+            local inst = gm.CInstance.instances_active[i]
+            if inst.object_index == gm.constants.oP and inst.user_name == username then
+                giveActor = inst
+                break
+            end
+        end
+    end
+
+    -- Item ID
+    if tonumber(item) then 
+        gm.item_take(giveActor, tonumber(item), tonumber(amount), false)
+        print( "taken "..amount.." "..item.." to "..username)
+        return
+    end
+
+    -- Item Name
+    local items = gm.variable_global_get("class_item")
+    for i=1, #items do
+        local lowerItemNameRequest = string.lower(item)
+        local lowerItemName = string.lower(items[i][2])
+
+        if lowerItemNameRequest == lowerItemName or string.find(lowerItemNameRequest, lowerItemName) or string.find(lowerItemName, lowerItemNameRequest) then
+            gm.item_take(giveActor, i-1, tonumber(amount), false)
+            print( "taken "..amount.." "..items[i][2].." to "..username)
+            return
+        end
+    end
+
+end
+
+functions['spawn_tp'] = function(actor)
+    gm.instance_create_depth(actor.x, actor.y, 1, gm.constants.oTrialsFinalShortcutTeleporter)
+end
+
+functions['kick'] = function(actor, username)
+    for i = 1, #gm.CInstance.instances_active do
+        local inst = gm.CInstance.instances_active[i]
+        if inst.object_index == gm.constants.oP and inst.user_name == username then
+            print(inst.user_name)
+            print(gm._mod_net_isAuthority(inst))
+            print(gm._mod_net_isHost(inst))
+            print(gm._mod_net_isClient(inst))
+            print(gm._mod_net_isOnline(inst))
+            -- gm.disconnect_player(inst)
+        end
+    end
 end
 
 functions['help'] = function(actor, command)
@@ -161,14 +298,35 @@ end)
 -- /give GoatHoof Umigatari 12
 -- /kill
 
+-- function match_command(actor, command)
+--     for command_name, pattern in pairs(match_strings) do
+--         if string.match(command, pattern['regex']) then
+--             functions[command_name] (actor, string.match(command, pattern['regex']))
+--             return 0
+--         end
+--     end
+--     if string.match(command, '/') then
+--         print("wrong command")
+--     end
+--     return 1
+-- end
+
 function match_command(actor, command)
     for command_name, pattern in pairs(match_strings) do
-        if string.match(command, pattern['regex']) then
-            functions[command_name] (actor, string.match(command, pattern['regex']))
-            return 0
+        local prefix = '/'..pattern['help_str']
+        if string.find(command,prefix) == 1 then
+            command = command:gsub(prefix, '')
+            local func_params = {}
+            for param in command:gmatch('([^%s]+)') do
+                func_params[#func_params+1] = param
+                print(param)
+            end
+            functions[command_name](actor, table.unpack(func_params))
+            return
         end
     end
-    if string.match(command, '/') then
+    
+    if string.find(command, '/') == 1 then
         print("wrong command")
     end
     return 1
@@ -180,4 +338,12 @@ end)
 
 gm.post_script_hook(gm.constants.draw_chat, function(self, other, result, args)
     --print(result.type)
+end)
+
+gm.post_script_hook(gm.constants.__input_system_tick, function()
+    if lvlToGive ~= 0 then
+        local director = gm._mod_game_getDirector()
+        director.player_exp = director.player_exp_required+0.1
+        lvlToGive = lvlToGive - 1
+    end
 end)
