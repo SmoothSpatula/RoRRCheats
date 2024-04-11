f5741e56dd8cc1135b3cc716e5c4e1f1be5ffe2b
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-04-11 21:06:12 +0200

    Create addfunc.lua

---
+++ addfunc.lua
@@ -0,0 +1,29 @@
+-- TestMod
+-- SmoothSpatula
+
+
+log.info("Successfully loaded ".._ENV["!guid"]..".")
+
+testmod = true
+str = "tettetette"
+
+--has access to passed args and normal parameters
+function public_func(args1, args2)
+    print(str)
+    print("printed this "..args2.." from testmod" )
+end
+
+mods.on_all_mods_loaded(function() 
+    --find chatconsole script
+    for k, v in pairs(mods) do 
+        if type(v) == "table" and v.chatconsole then 
+            ChatConsole = v 
+        end
+    end 
+    --add the function you want to add
+    for k, v in pairs(mods) do
+        if type(v) == "table" and v.testmod then 
+            ChatConsole.add_function(public_func, v.public_func, "<y> lolxdtest", "public")
+        end 
+    end
+end)
