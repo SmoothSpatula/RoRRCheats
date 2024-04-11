ef22cc678adda55fd5c8253a1c15063b39af31d6
Miguel <39136142+LoveBetween@users.noreply.github.com>
2024-04-11 03:21:22 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -12,29 +12,106 @@ o_user = '%s*([^%s]*)%s*' -- optional username
 m_user = '%s*([^%s]+)%s*' -- mandatory username
 o_nb = '%s*(%d*)%s*' -- optional number
 m_nb = '%s*(%d+)%s*' -- mandatory number
-wrd = '%s*(%a+)%s*' -- word
+m_wrd = '%s*(%a+)%s*' -- mandatory word
+o_wrd = '%s*(%a*)%s*' -- mandatory word
 
 
 -- <> is a mandatory field
 -- [] is an optional field
 match_strings = {
-    kill_username = '/kill'..o_user, -- /kill [username]
-    give_item_id = '/give'..o_nb..m_nb..o_user, -- /give [amount] <item_id> [username] 
-    give_item_name = '/give'..o_nb..wrd..o_user, -- /give [amount] <item_name>  [username] 
-    remove_item_id = '/remove'..o_nb..m_nb..o_user, -- /remove [amount] <item_id> [username]
-    remove_item_name = '/remove'..o_nb..wrd..o_user, -- /remove  [amount] <item_name> [username]
-    set_field = '/set'..wrd..m_nb..o_user, -- /set <field_name> <value> [username]
-    set_skill_id = '/set'..m_nb..m_nb..o_user, -- /skill <bar_slot> <skill_id> [username]
-    set_skill_name = '/set'..m_nb..wrd..o_user, -- /skill <bar_slot> <skill_name> [username]
-    spawn_tp = '/spawntp', -- /spawntp
-    kick = '/kick'..m_user
+    kill_username = { 
+        regex = '/kill'.. o_user, 
+        usage =  '<y>/kill <b>username',
+        help_str = 'kill'
+    },
+    give_item_id = { 
+        regex = '/give'..o_nb..m_nb..o_user, 
+        usage = '<y>/give <b>amount <w>item_id <b>username',
+        help_str = 'give'
+    },
+    give_item_name = { 
+        regex = '/give'..o_nb..m_wrd..o_user, 
+        usage = '<y>/give <b>amount <w>item_name <b>username',
+        help_str = 'give'
+    },
+    remove_item_id = { 
+        regex = '/remove'..o_nb..m_nb..o_user, 
+        usage = '<y>/remove <b>amount <w>item_id <b>username',
+        help_str = 'remove'
+    },
+    remove_item_name = { 
+        regex = '/remove'..o_nb..m_wrd..o_user, 
+        usage = '<y>/remove <b>amount <w>item_name <b>username',
+        help_str = 'remove'
+    },
+    set_field = { 
+        regex = '/set'..m_wrd..m_nb..o_user, 
+        usage = '<y>/set field_name <w>value <b>username',
+        help_str = 'set'
+    },
+    set_skill_id = { 
+        regex = '/skill'..m_nb..m_nb..o_user, 
+        usage = '<y>/skill <w>bar_slot <w>skill_id <b>username',
+        help_str = 'set'
+    },
+    set_skill_name = { 
+        regex = '/skill'..m_nb..m_wrd..o_user, 
+        usage = '<y>/skill <w>bar_slot <w>skill_name <b>username',
+        help_str  = 'skill'
+    },
+    spawn_tp = { 
+        regex = '/spawntp', 
+        usage = '<y>/spawntp',
+        help_str = 'spawntp'
+    },
+    kick = { 
+        regex = '/kick'..m_user, 
+        usage = '<y>/kick <b>username',
+        help_str = 'kick'
+    },
+    help = { 
+        regex = '/help'..o_user, --using this so people can type /help /give
+        usage = '<y>/help <b>username',
+        help_str = 'help'
+    }
 }
+
+
+-- ========== Utils ==========
+local function add_chat_message(text)
+    gm.chat_add_message(gm["@@NewGMLObject@@"](gm.constants.ChatMessage, text))
+end
+
+
+local function help_print_all()
+    local s = "<r>List of all commands <w>: "
+    for command_name, pattern in pairs(match_strings) do
+        s = s.."/"..pattern.help_str.." "
+    end
+    add_chat_message(s)
+end
+
+local function help_print_command(command)
+    local s = '<r>Usage <w> : Mandatory <b> Optional\n'
+    local matched_commands = 0
+    for command_name, pattern in pairs(match_strings) do
+        if string.find(string.lower(command), pattern.help_str) ~= nil then
+            if matched_commands>0 then s = s..'\n' end
+            s = s..pattern.usage
+            matched_commands = matched_commands + 1
+        end
+    end
+    add_chat_message(s)
+    if matched_commands<1 then 
+        add_chat_message("<r> Did not recognize command")
+        help_print_all()
+    end
+end
 
 -- ========== Functions Associated With Commands ==========
 
 functions = {
     kill_username = function(actor, username)
-        
         if username == '' then
             print(actor.user_name.. " killed themselves.")
         else
@@ -50,6 +127,14 @@ functions['give_item_name'] = function(actor, amount, item_name, username)
     print( "given "..amount.." "..item_name.." to "..username)
 end
 
+functions['help'] = function(actor, command)
+    if command == '' or command == nil then 
+        help_print_all() 
+    return end
+    help_print_command(command)
+end
+
+        
 -- ========== ImGui ==========
 
 -- Console in ImGui, Work In Progress
@@ -72,10 +157,13 @@ end)
 
 function match_command(actor, command)
     for command_name, pattern in pairs(match_strings) do
-        if string.match(command, pattern) then
-            functions[command_name] (actor, string.match(command, pattern))
+        if string.match(command, pattern['regex']) then
+            functions[command_name] (actor, string.match(command, pattern['regex']))
             return 0
         end
+    end
+    if string.match(command, '/') then
+        print("wrong command")
     end
     return 1
 end
@@ -86,7 +174,4 @@ end)
 
 gm.post_script_hook(gm.constants.draw_chat, function(self, other, result, args)
     --print(result.type)
-end)
-gm.post_script_hook(gm.constants.chat_add_user_message, function(self, other, result, args)
-    match_command(args[1].value, args[2].value) -- pass actor instance and message
 end)
